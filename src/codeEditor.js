61eff813107d7eb35389160ffa5dce90c6c5a438
Kyle Brock <egmicecake@gmail.com>
2018-05-16 14:41:28 -0400

    Update

---
+++ src/codeEditor.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './codeEditor.css';
+
+export default class CodeEditor extends React.Component{
+  render(){
+    return(
+      <p> This is a test</p>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CodeEditor from './codeEditor';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<CodeEditor />, document.getElementById('root'));
 registerServiceWorker();
