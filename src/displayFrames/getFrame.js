993cc9df5c9e39d7f805e4b1d8b354f7168d64ec
Kyle Brock <egmicecake@gmail.com>
2018-05-21 12:09:12 -0400

    Update

--- src/displayFrames/getFrame.js
+++ src/displayFrames/getFrame.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import './frame.css';
 import Axios from 'axios'
+import ResultFrame from './resultFrame';
 
 export default class GetFrame extends React.Component{
   /**Default Constructor*/
   constructor(){
     super();
     this.state = {
-      disabled: true,
+      disabled: false,
+      txtDisabled: true,
       boardID: "",
       key: "",
       token: "",
       btnName: "",
       hidden: true,
+      display: false,
+      data: null,
+      test: false,
     };
   }
 
@@ -22,7 +27,7 @@ export default class GetFrame extends React.Component{
     if(event.target.selectedIndex === 1){
       //Setting state
       this.setState({
-        disabled: false,
+        txtDisabled: false,
         boardID: "",
         key: "",
         token: "",
@@ -32,7 +37,7 @@ export default class GetFrame extends React.Component{
     }else{
       //Setting state
       this.setState({
-        disabled: false,
+        txtDisabled: false,
         boardID: "",
         key: "",
         token: "",
@@ -40,6 +45,10 @@ export default class GetFrame extends React.Component{
         hidden: false,
       });
     }
+  }
+
+  test = () => {
+    this.setState({test: true});
   }
 
   /**update Function*/
@@ -57,6 +66,25 @@ export default class GetFrame extends React.Component{
     }
   }
 
+  /**displayResults Function*/
+  displayResults = () => {
+    //Declaring fields
+    var display = this.state.display;
+    var results;
+
+    //Checking display boolean
+    if(display === true){
+      //Setting frame
+      results = (<ResultFrame data = {this.state.data}/>);
+    }
+    if(this.state.test === true){
+      results = (<ResultFrame />);
+    }
+
+    //Returning result frame
+    return results;
+  }
+
   /**getData Function*/
   getData = () => {
     //Declaring fields
@@ -69,6 +97,13 @@ export default class GetFrame extends React.Component{
 
     Axios.get(url).then(res => {
       console.log(res.data);
+      //Setting state
+      this.setState({
+        display: true,
+        data: res.data,
+        disabled: true,
+        txtDisabled: true,
+      });
     });
   }
 
@@ -83,7 +118,7 @@ export default class GetFrame extends React.Component{
             <tr>
               <td className = "left"> What component?</td>
               <td>
-                <select defaultValue = "Choose Component" onChange = {this.enable}>
+                <select defaultValue = "Choose Component" disabled = {this.state.disabled} onChange = {this.enable}>
                   <option disabled hidden> Choose Component</option>
                   <option> Lists</option>
                   <option> Cards</option>
@@ -93,28 +128,46 @@ export default class GetFrame extends React.Component{
             <tr>
               <td className = "left"> Board ID</td>
               <td>
-                <input type = "text" id = "txtBoard" value = {this.state.boardID} disabled = {this.state.disabled} onChange = {this.update}/>
+                <input type = "text"
+                  id = "txtBoard"
+                  value = {this.state.boardID}
+                  disabled = {this.state.txtDisabled}
+                  onChange = {this.update}/>
               </td>
             </tr>
             <tr>
               <td className = "left"> Key</td>
               <td>
-                <input type = "text" id = "txtKey" value = {this.state.key} disabled = {this.state.disabled} onChange = {this.update}/>
+                <input type = "text"
+                  id = "txtKey"
+                  value = {this.state.key}
+                  disabled = {this.state.txtDisabled}
+                  onChange = {this.update}/>
               </td>
             </tr>
             <tr>
               <td className = "left"> Token</td>
               <td>
-                <input type = "text" id = "txtToken" value = {this.state.token} disabled = {this.state.disabled} onChange = {this.update}/>
+                <input type = "text"
+                  id = "txtToken"
+                  value = {this.state.token}
+                  disabled = {this.state.txtDisabled}
+                  onChange = {this.update}/>
               </td>
             </tr>
             <tr>
               <td className = "center" colSpan = "2">
-                <input type = "button" hidden = {this.state.hidden} value = {this.state.btnName} onClick = {() => {this.getData()}}/>
+                <input type = "button"
+                  disabled = {this.state.disabled}
+                  hidden = {this.state.hidden}
+                  value = {this.state.btnName}
+                  onClick = {() => {this.getData()}}/>
               </td>
             </tr>
           </tbody>
         </table>
+        <input type = "button" value = "test" onClick = {this.test}/>
+        {this.displayResults()}
       </div>
     );
   }
---
+++ src/displayFrames/resultFrame.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './frame.css';
+
+export default class ResultFrame extends React.Component{
+  /**Rendering Webpage*/
+  render(){
+    return(
+      <div>
+        <br />
+
+      </div>
+    );
+  }
+}
